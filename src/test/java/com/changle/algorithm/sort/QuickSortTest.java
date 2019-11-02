b1528391d2abcdd5fdc2490dfc05553385986c3e
changle <changle@stu.xmu.edu.cn>
2019-11-02 09:12:17 +0800

    快速排序2.0

--- src/test/java/com/changle/algorithm/sort/QuickSortTest.java
+++ src/test/java/com/changle/algorithm/sort/QuickSortTest.java
@@ -1,6 +1,7 @@
 package com.changle.algorithm.sort;
 
 import com.changle.algorithm.sort.impl.QuickSort;
+import com.changle.algorithm.sort.impl.QuickSort2;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -16,41 +17,67 @@ public class QuickSortTest {
 
     @Test
     public void testSort() {
+        // 测试次数
         int times = 10000;
+        // 测试数组长度
         int length = 10000;
+        // 测试数组中最大值
+        int max = 10000;
         boolean equals = true;
-        Sort sort = new QuickSort();
+        Sort sort1 = new QuickSort();
+        Sort sort2 = new QuickSort2();
+        int[] array = getArray(length, max);
+
+        long startTime1 = System.currentTimeMillis();
         for (int i = 1; i <= times; i++) {
 //            System.out.println("---------第" + i + "次-----------");
-            if (!testSortOnce(length, sort)) {
+            if (!testSortOnce(array, sort1)) {
                 equals = false;
                 break;
             }
         }
         System.out.println("最终结果是否全部相等：" + equals);
+        long endTime1 = System.currentTimeMillis();
+        System.out.println(endTime1 - startTime1);
+
+        long startTime2 = System.currentTimeMillis();
+        for (int i = 1; i <= times; i++) {
+//            System.out.println("---------第" + i + "次-----------");
+            if (!testSortOnce(array, sort2)) {
+                equals = false;
+                break;
+            }
+        }
+        System.out.println("最终结果是否全部相等：" + equals);
+        long endTime2 = System.currentTimeMillis();
+        System.out.println(endTime2 - startTime2);
     }
 
-    public boolean testSortOnce(int length, Sort sort) {
+    public int[] getArray(int length, int max) {
         int[] array = new int[length];
-        int[] array1 = new int[length];
-        int[] array2 = new int[length];
         Random random = new Random();
         for (int j = 0; j < array.length; j++) {
-            array[j] = random.nextInt(10000);
+            array[j] = random.nextInt(max);
         }
+        return array;
+    }
+
+    public boolean testSortOnce(int[] array, Sort sort) {
+        int[] array1 = new int[array.length];
+        int[] array2 = new int[array.length];
 
 //        System.out.println("*********************************");
 //        System.out.println("原数组：" + Arrays.toString(array));
 //        System.out.println("*********************************");
 
 //        System.out.println("*********************************");
-        System.arraycopy(array, 0, array1, 0, length);
+        System.arraycopy(array, 0, array1, 0, array.length);
         sort.sort(array1);
 //        System.out.println("自定义排序：" + Arrays.toString(array1));
 //        System.out.println("*********************************");
 
 //        System.out.println("*********************************");
-        System.arraycopy(array, 0, array2, 0, length);
+        System.arraycopy(array, 0, array2, 0, array.length);
         Arrays.sort(array2);
 //        System.out.println("系统排序：" + Arrays.toString(array2));
 //        System.out.println("*********************************");

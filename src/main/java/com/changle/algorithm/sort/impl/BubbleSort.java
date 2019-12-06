a565a9fe02b82daceb8c7286426082db15d6f659
changle <changle@stu.xmu.edu.cn>
2019-12-06 11:56:37 +0800

    优化

--- src/main/java/com/changle/algorithm/sort/impl/BubbleSort.java
+++ src/main/java/com/changle/algorithm/sort/impl/BubbleSort.java
@@ -19,9 +19,11 @@ public class BubbleSort implements Sort {
             return;
         }
         // 开始比较
+        // 变量i可以看做用于控制比较次数，也可以理解为控制每次待排序的数组长度
         for (int i = 1; i < array.length; i++) {
-            //设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成
+            // 设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成
             boolean flag = true;
+            // 变量j用于控制一次比较中需要比较的元素个数
             for (int j = 0; j < array.length - i; j++) {
                 if (array[j] > array[j + 1]) {
                     swap(array, j, j + 1);
---
+++ src/main/java/com/changle/algorithm/sort/impl/InsertionSort.java
@@ -0,0 +1,18 @@
+package com.changle.algorithm.sort.impl;
+
+import com.changle.algorithm.sort.Sort;
+
+/**
+ * 插入排序.
+ * 将数组分为无序区与有序区，每次从无序区挑一个插入到有序区直至结束
+ *
+ * @author changle
+ * @version 1.0.0
+ * @date 2019/12/06
+ **/
+public class InsertionSort implements Sort {
+    @Override
+    public void sort(int[] array) {
+
+    }
+}
